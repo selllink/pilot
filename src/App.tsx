83a349ee961d6d7ad033244d43440d93c0355034
Fernando Suarez <fernando@goalsetter.co>
2026-02-17 10:57:14 -0300

    Implement key-based rendering for EditPage in routing; enhance component reactivity with useLocation hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import { Layout } from './components/layout/Layout'
 import { CreatePage } from './pages/CreatePage'
 import { ViewPage } from './pages/ViewPage'
 import { SuccessPage } from './pages/SuccessPage'
 import { DashboardPage } from './pages/DashboardPage'
 import { EditPage } from './pages/EditPage'
+
+function EditPageWithKey() {
+  const location = useLocation()
+  return <EditPage key={location.key} />
+}
 
 function App() {
   return (
@@ -15,7 +20,7 @@ function App() {
         <Route path="/create/success" element={<SuccessPage />} />
         <Route path="/v/:shortSlug" element={<ViewPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/dashboard/edit/:id" element={<EditPage />} />
+        <Route path="/dashboard/edit/:id" element={<EditPageWithKey />} />
       </Route>
     </Routes>
   )
